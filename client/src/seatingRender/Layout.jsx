65ff18ad49ff335f3124e7e4b9d0c4114eb28f35
linaeb <lina@linabe.se>
2019-01-11 14:31:15 +0100

    solved table render

--- client/src/seatingRender/Layout.jsx
+++ client/src/seatingRender/Layout.jsx
@@ -1,24 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Table from "./Table";
 import "../css/table.css";
+import ChairContainer from "./ChairContainer";
 
 class Layout extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-}}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() { 
-        return (
-
-            <div className="box">
-            <Table participants={this.props.participants}>
-            </Table>
-
-            </div>
-
-          );
-    }
+  render() {
+    return (
+      <div className="box">
+        {this.props.participants.map((rowdata, i) => (
+          <Table key={i} participants={rowdata} />
+        ))}
+        
+      </div>
+    );
+  }
 }
- 
 export default Layout;
+
+// {this.props.participants.map(participant => (
+//     <span key={participant.id}>
+//       <Chair name={participant.name}></Chair>
+//     </span>
+//   ))}
