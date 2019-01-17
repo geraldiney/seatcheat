25d29770160d4b96d6a1d51d5fa27ef1caf65014
LouiseHoll <louise.hollingworth@gmail.com>
2019-01-17 17:23:01 +0100

    fixat whiteboard på seat render

--- client/src/seatingRender/Layout.jsx
+++ client/src/seatingRender/Layout.jsx
@@ -3,25 +3,20 @@ import Table from "./Table";
 import "../css/layout.css";
 
 class Layout extends Component {
-
   render() {
-
-
-
     return (
+      <div>
+        <div className="teacher-lg" />
 
-      
-      <div className="box">
-        {this.props.participants.map((rowdata, i) => (
-          <div key={i}>
-          <Table participants={rowdata} />
-          </div>
-        ))}
-        
+        <div className="box">
+          {this.props.participants.map((rowdata, i) => (
+            <div key={i}>
+              <Table participants={rowdata} />
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
 }
 export default Layout;
-
-
